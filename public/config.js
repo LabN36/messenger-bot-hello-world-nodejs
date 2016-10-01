ab90c13ef9fbee3ed1b30f79c1444d658a234782
Rishabh Agrawal <ragrawal804@gmail.com>
2016-10-02 02:40:44 +0530

    initial messenger commit

---
+++ public/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+
+  appSecret: "5caff60de98ec0aa4450963370e97bac",
+  pageAccessToken: "EAAQtdn0lalkBAAwu3E09jZAZAc2Dz1OIe3cNEpcPdGP27ibvraeZA7vxkx7tSSZCFNGT8SSZCPaH6BGXeyGvKfv9OT9uR7lbsAZCLF1vrZCYtWakuwWps23m5tZCK4RtK1J6dxbX6OYhr3uZAbZCMmHFyyQdKiUaR8nJFaLpeGPVZBvkwZDZD", 
+ validationToken: "helloworldmessengerbot",
+ 
+};
