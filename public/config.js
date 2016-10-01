07e1690ffcef1f4aa23ab29e3fec52b4ab061e01
Rishabh Agrawal <ragrawal804@gmail.com>
2016-10-02 03:39:03 +0530

    messenger hello world code

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
 
-  appSecret: "5caff60de98ec0aa4450963370e97bac",
-  pageAccessToken: "EAAQtdn0lalkBAM6jC9QqZANn1yAy42xCegZCz8ZB1gHu6T6BfxRkgoFjYKJY7fZAycTn2YNuoPVW07HtbOlP4ZAWoSJcaA0B6yRtazdnvoN7s7gQrq7QBwhlRY5p6KOPzSxfuRzxpx7m57F2kuYvf9CaHgnOto56wW5HMTgjq3gZDZD", 
- validationToken: "helloworldmessengerbot",
+  appSecret: "enter your app secret here",
+  pageAccessToken: "enter your page access token here", 
+ validationToken: "enter your webhook validation token here",
  
 };
